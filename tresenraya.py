05106d39dabffe694bae14eb0f8a7ad4d2961a79
unknown <2btb03@IESRAMONLLULL.LOCAL>
2023-02-02 13:06:15 +0100

    tres

--- tresenraya.py
+++ tresenraya.py
@@ -1,47 +1,81 @@
 
 import random
 casillas = ["","","","","","","","",""]
+casillas_libres = [0,1,2,3,4,5,6,7,8]
 
-posicion = int(input("Ahora jugaremos al tres en raya, elige una casilla del 0 al 8"))
-player = "x"
-casillas[posicion]= player
+player = input("¿Hola jugador, quieres ser x/o? ")
+if player == "x":
+    pc = "o"
+if player == "o":
+    pc = "x"
 
-if casillas[0] == casillas[1] == casillas[2] == player:
-    print("Has ganado!!!")
-if casillas[3] == casillas[4] == casillas[5] == player:
-    print("Has ganado!!!")
-if casillas[6] == casillas[7] == casillas[8] == player:
-    print("Has ganado!!!")
-if casillas[0] == casillas[3] == casillas[6] == player:
-    print("Has ganado!!!")
-if casillas[1] == casillas[4] == casillas[7] == player:
-    print("Has ganado!!!")
-if casillas[2] == casillas[5] == casillas[8] == player:
-    print("Has ganado!!!")
-if casillas[0] == casillas[4] == casillas[8] == player:
-    print("Has ganado!!!")
-if casillas[2] == casillas[4] == casillas[6] == player:
-    print("Has ganado!!!")
+final = False
 
+while not final:
 
-if casillas[0] == casillas[1] == casillas[2] == pc:
-    print("Has perdido...")
-if casillas[3] == casillas[4] == casillas[5] == pc:
-    print("Has perdido...")
-if casillas[6] == casillas[7] == casillas[8] == pc:
-    print("Has perdido...")
-if casillas[0] == casillas[3] == casillas[6] == pc:
-    print("Has perdido")
-if casillas[1] == casillas[4] == casillas[7] == pc:
-    print("Has perdido!!!")
-if casillas[2] == casillas[5] == casillas[8] == pc:
-    print("Has perdido...")
-if casillas[0] == casillas[4] == casillas[8] == pc:
-    print("Has perdido...")
-if casillas[2] == casillas[4] == casillas[6] == pc:
-    print("Has perdido...")
+    posicion = int(input("Ahora jugaremos al tres en raya, elige una casilla del 0 al 8"))
+    casillas[posicion]= player
+    casillas_libres.remove(posicion)
+
+    if casillas[0] == casillas[1] == casillas[2] == player:
+        print("Has ganado!!!") 
+        final = True
+    if casillas[3] == casillas[4] == casillas[5] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[6] == casillas[7] == casillas[8] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[0] == casillas[3] == casillas[6] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[1] == casillas[4] == casillas[7] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[2] == casillas[5] == casillas[8] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[0] == casillas[4] == casillas[8] == player:
+        print("Has ganado!!!")
+        final = True
+    if casillas[2] == casillas[4] == casillas[6] == player:
+        print("Has ganado!!!")
+        final = True
+
+posicion = random.choice(casillas_libres)
+casillas[posicion] = pc
+casillas_libres.remove(posicion)
+
+    if casillas[0] == casillas[1] == casillas[2] == pc:
+        print("Has perdido...")
+        final = True
+    if casillas[3] == casillas[4] == casillas[5] == pc:
+        print("Has perdido...")
+        final = True
+    if casillas[6] == casillas[7] == casillas[8] == pc:
+        print("Has perdido...")
+        final = True
+    if casillas[0] == casillas[3] == casillas[6] == pc:
+        print("Has perdido")
+        final = True
+    if casillas[1] == casillas[4] == casillas[7] == pc:
+        print("Has perdido!!!")
+        final = True
+    if casillas[2] == casillas[5] == casillas[8] == pc:
+        print("Has perdido...")
+        final = True
+    if casillas[0] == casillas[4] == casillas[8] == pc:
+        print("Has perdido...")
+        final = True
+    if casillas[2] == casillas[4] == casillas[6] == pc:
+        print("Has perdido...")
+        final = True
 
 
 print("|"+casillas[0]+"|"+casillas[1]+"|"+casillas[2]+"|")
 print("|"+casillas[3]+"|"+casillas[4]+"|"+casillas[5]+"|")
 print("|"+casillas[6]+"|"+casillas[7]+"|"+casillas[8]+"|")
+
+if casillas_libres == []:
+    print(Empate)
+    final = True
